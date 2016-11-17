ddc088bf142adf662c572542a542ac113fdb7066
Kanitkorn S <k.sujautra@gmail.com>
2016-11-17 18:20:16 +0700

    Fetch data from pages and append to body

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('test')
+/* eslint-env browser */
+
+'use strict'
+
+fetch('http://localhost:8080/pages')
+  .then(res => res.text())
+  .then(text => {
+    const div = document.createElement('div')
+
+    div.innerHTML = text
+
+    const body = document.getElementsByTagName('body')[0]
+    body.appendChild(div)
+  })
