67cf63a6fef771f2516ca5fb39a5c9384cf978eb
Kanitkorn S <k.sujautra@gmail.com>
2016-11-21 09:44:24 +0700

    Add script for async loading another script

--- index.js
+++ index.js
@@ -2,13 +2,38 @@
 
 'use strict'
 
-fetch('http://localhost:8080/pages')
-  .then(res => res.text())
-  .then(text => {
-    const div = document.createElement('div')
+// Ref: https://www.html5rocks.com/en/tutorials/speed/script-loading/
+const scriptURL = 'http://localhost:8080/pages/dist/bundle.js'
+const pendingScripts = []
+const firstScript = document.scripts[0]
 
-    div.innerHTML = text
+// Watch scripts load in IE
+function stateChange () {
+  // Execute as many scripts in order as we can
+  let pendingScript
+  while (pendingScripts[0] && pendingScripts[0].readyState === 'loaded') {
+    pendingScript = pendingScripts.shift()
+    // avoid future loading events from this script (eg, if src changes)
+    pendingScript.onreadystatechange = null
+    // can't just appendChild, old IE bug if element isn't closed
+    firstScript.parentNode.insertBefore(pendingScript, firstScript)
+  }
+}
 
-    const body = document.getElementsByTagName('body')[0]
-    body.appendChild(div)
-  })
+if ('async' in firstScript) { // modern browsers
+  const script = document.createElement('script')
+  script.async = false
+  script.src = scriptURL
+  document.head.appendChild(script)
+} else if (firstScript.readyState) { // IE<10
+  // create a script and add it to our todo pile
+  const script = document.createElement('script')
+  pendingScripts.push(script)
+  // listen for state changes
+  script.onreadystatechange = stateChange
+  // must set src AFTER adding onreadystatechange listener
+  // else we’ll miss the loaded event for cached scripts
+  script.src = scriptURL
+} else { // fall back to defer
+  document.write('<script src="' + scriptURL + '" defer></' + 'script>')
+}
