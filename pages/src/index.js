d2d4ebcda67cb83276dc2c3c6507fddf0660314e
Kanitkorn S <k.sujautra@gmail.com>
2016-11-22 15:01:21 +0700

    Style loaded widget

--- pages/src/index.js
+++ pages/src/index.js
@@ -12,8 +12,26 @@ render((
   <div className={style.container}>
     <div className={style.header}>
       <img className={style.icon} src={icon} />
+      <span className={style.title}>Recent Feedback</span>
+      <span className={style.arrow} />
+    </div>
+    <div className={style.body}>
+      <div className={style.navLeftContainer}>
+        <span className={style.navLeft} />
+      </div>
+      <div className={style.slideContainer}>
+        <div className={style.slideContent}>
+          <p className={style.content}>As always Mark was extremely responsive, kind, polite and professional in his handling of this ticket. I only have good things to say about Mark's delivery! ...</p>
+          <p className={style.name}>Mark Z.</p>
+          <p className={style.date}>November 1, 2016</p>
+        </div>
+      </div>
+      <div className={style.navRightContainer}>
+        <span className={style.navRight} />
+      </div>
     </div>
     <div className={style.footer}>
+      <span className={style.poweredBy}>Powered by</span>
       <img className={style.logo} src={logo} />
     </div>
   </div>
